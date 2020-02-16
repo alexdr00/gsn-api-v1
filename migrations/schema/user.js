a9065faee6dc22263d41e01d2d2e4b6d8e472b41
alexdr00 <alexrendon2109@gmail.com>
2020-02-16 15:16:31 -0500

    Update user schema

--- migrations/schema/user.js
+++ migrations/schema/user.js
@@ -5,9 +5,7 @@ const userMigration = ({ db }) => {
     const query = `
       CREATE TABLE IF NOT EXISTS user (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        name CHAR(255) NOT NULL,
         email CHAR(255) NOT NULL UNIQUE,
-        password CHAR(255) NOT NULL,
         budget DECIMAL(8, 2) NOT NULL DEFAULT 0,
         deleted_at datetime,
         updated_at datetime
