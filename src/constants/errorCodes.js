6f61d991376f47341e7a3ddf92ab08ee503f6855
alexdr00 <alexrendon2109@gmail.com>
2020-02-22 13:33:44 -0500

    Add error codes to constants.

---
+++ src/constants/errorCodes.js
@@ -0,0 +1,7 @@
+module.exports = {
+  SESSION_EXPIRED: 'SessionExpiredError',
+  TOKEN_EXPIRED: 'TokenExpiredError',
+  NOT_AUTHORIZED: 'NotAuthorizedException',
+  USERNAME_EXISTS: 'UsernameExistsException',
+  USERNAME_NOT_CONFIRMED: 'UserNotConfirmedException',
+};
